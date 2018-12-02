12aa9d7f4b9f3006bb9662b7793cd1b836b45969
radiand <radiand@protonmail.com>
2018-12-02 15:51:33 +0100

    fixed separation of server and gameserver

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,9 +1,15 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
 #[macro_use] extern crate log;
 extern crate simplelog;
+#[macro_use] extern crate rocket;
 
 use simplelog::{TermLogger, LevelFilter, Level, Config};
 
-mod ecs;
+#[get("/")]
+fn index() -> &'static str {
+    "here we'll serve client application"
+}
 
 fn main() {
     let logger_config = Config {
@@ -14,4 +20,6 @@ fn main() {
         time_format: Some("%T%.3f") };
     let _ = TermLogger::init(LevelFilter::Trace, logger_config);
     info!("server initialized");
+
+    rocket::ignite().mount("/", routes![index]).launch();
 }
