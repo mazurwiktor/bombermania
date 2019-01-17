5c61bb2104a4e3ab5773f74deaa7ec32fe988670
Wiktor Mazur <wiktormazur1@gmail.com>
2019-01-17 20:46:58 +0100

    speed inlined into velocity calculation

--- client/src/game/system/input.rs
+++ client/src/game/system/input.rs
@@ -7,28 +7,28 @@ pub fn input_system(game_state: &mut GameState) {
     if let Some(input) = &game_state.input {
         if let Some(player) = &mut game_state.entities[game_state.player] {
             if let Some(physics) = &mut player.physics {
-                physics.position += get_velocity(&input, game_state.time_elapsed) * physics.speed;
+                physics.position += get_velocity(&input, physics.speed, game_state.time_elapsed);
             }
         }
     }
 }
 
-fn get_velocity(input: &Input, time_elapsed: f64) -> Vec2D {
+fn get_velocity(input: &Input, speed: f64, time_elapsed: f64) -> Vec2D {
     let mut velocity = Vec2D::new(0.0, 0.0);
     if input.key_pressed.contains(&Key::UpArrow) {
-        velocity += Vec2D::new(0.0, -1.0);
+        velocity += Vec2D::new(0.0, -speed);
     }
 
     if input.key_pressed.contains(&Key::DownArrow) {
-        velocity += Vec2D::new(0.0, 1.0);
+        velocity += Vec2D::new(0.0, speed);
     }
 
     if input.key_pressed.contains(&Key::LeftArrow) {
-        velocity += Vec2D::new(-1.0, 0.0);
+        velocity += Vec2D::new(-speed, 0.0);
     }
 
     if input.key_pressed.contains(&Key::RightArrow) {
-        velocity += Vec2D::new(1.0, 0.0);
+        velocity += Vec2D::new(speed, 0.0);
     }
 
     velocity * time_elapsed
