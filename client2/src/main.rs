0db756bfabf42d54f3fbda8d07f01135e27c4f56
radiand <radiand@protonmail.com>
2019-05-30 23:07:02 +0200

    stdweb client can talk with backend

--- client2/src/main.rs
+++ client2/src/main.rs
@@ -5,28 +5,50 @@ use stdweb::unstable::TryInto;
 use stdweb::web::{
     document,
     window,
-    CanvasRenderingContext2d
+    CanvasRenderingContext2d,
+    WebSocket
 };
 
 use stdweb::web::event::{
     MouseMoveEvent,
     ResizeEvent,
+    KeyPressEvent,
 };
 
 use stdweb::web::html_element::CanvasElement;
+use stdweb::web::html_element::InputElement;
 
 // Shamelessly stolen from webplatform's TodoMVC example.
 macro_rules! enclose {
     ( ($( $x:ident ),*) $y:expr ) => {
         {
             $(let $x = $x.clone();)*
-            $y
+                $y
         }
     };
 }
 
+fn attach_ws() {
+    let ws = WebSocket::new_with_protocols(
+        "ws://127.0.0.1:8081", &vec!["rust-websocket"]).unwrap();
+
+    let text_entry: InputElement = document().query_selector( ".form input" ).unwrap().unwrap().try_into().unwrap();
+    text_entry.add_event_listener( enclose!( (text_entry) move |event: KeyPressEvent| {
+        if event.key() == "Enter" {
+            event.prevent_default();
+
+            let text: String = text_entry.raw_value();
+            if text.is_empty() == false {
+                text_entry.set_raw_value("");
+                ws.send_text(&text).unwrap();
+            }
+        }
+    }));
+}
+
 fn main() {
     stdweb::initialize();
+    attach_ws();
 
     let canvas: CanvasElement = document().query_selector( "#canvas" ).unwrap().unwrap().try_into().unwrap();
     let context: CanvasRenderingContext2d = canvas.get_context().unwrap();
@@ -39,10 +61,9 @@ fn main() {
         canvas.set_height(canvas.offset_height() as u32);
     }));
 
-    canvas.add_event_listener( enclose!( (context) move |event: MouseMoveEvent| {
-        context.fill_rect(f64::from(event.client_x() - 5), f64::from(event.client_y() - 5)
-                          , 10.0, 10.0);
-    }));
+    //canvas.add_event_listener( enclose!( (context) move |event: MouseMoveEvent| {
+    //    context.fill_rect(f64::from(event.client_x() - 5), f64::from(event.client_y() - 5), 10.0, 10.0);
+    //}));
 
     stdweb::event_loop();
 }
